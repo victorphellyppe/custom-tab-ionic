2c10a028ae28b51bd0b691ae7d1514dcf9f906b7
Victor Oliveira <vitinifal@gmail.com>
2024-11-16 15:21:20 -0300

    Commit with tabs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,16 @@ export class HomePage {
 
   constructor() {}
 
+  ionViewDidEnter() {
+    const tabButton = document.querySelector('#tab-button-tab3');
+
+    if (tabButton && tabButton.shadowRoot) {
+      const buttonNative = tabButton.shadowRoot.querySelector('.button-native');
+
+      if (buttonNative) {
+        buttonNative.setAttribute('style', 'margin-top: -2px');
+      }
+    }
+  }
+
 }
